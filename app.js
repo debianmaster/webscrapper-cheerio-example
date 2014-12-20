2b616d049e11f0c995537ef7f0f885eab69e0d2f
debianmaster <cjonagam@equinix.com>
2014-12-20 11:43:25 -0800

    2nd commit

--- app.js
+++ app.js
@@ -4,32 +4,57 @@
 
 var request = require('request');
 var $ = require('cheerio');
+var requestjson = require('request-json');
+var client = requestjson.newClient('http://i63.in:8090');
 var async = require('async');
 
 var movies =[];
+
 request('http://www.oldtelugusongs.com/cgi-bin/search2/search.pl?group=1', function (error, response, body) {
     if (!error && response.statusCode == 200) {
         var body = $.load(body);
-        //console.log(body('table').html());
         var table = $.load(body('table').html());
         table('tr').each(function(i,tr){
             var movie={};
             movie.songs=[];
             movie.movieName=$(this).find('td').eq(1).text();
             movie.movieId=null;
+            movie.coverPage=null;
             var song={};
             song.url=$(this).find('a').attr('href');
             song.title=$(this).find('td').eq(0).text();
+            song.writtenBy=$(this).find('td').eq(4).text();
+            song.year=$(this).find('td').eq(5).text();
+            song.mDirector=$(this).find('td').eq(3).text();
             song.singers=[];
             $(this).find('td').eq(2).find('a').each(function(i,singer){
                song.singers.push($(this).text())
             });
             movie.songs.push(song);
-            movies.push(movie);
+            if(movies[movie.movieName]==undefined) {
+                movies[movie.movieName] = movie;
+            }
+            else {
+                movies[movie.movieName].songs.push(song);
+            }
         });
-        for(var movie in movies){
-            console.log(movies[movie]);
+        //console.log("--------------------------------",movies,"^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+        for(var k in movies){
+            console.log(movies[k]);
+            client.post("/movies",movies[k],function(err,data){
+
+            });
         }
+        /*
+        async.each(movies,function(item, callback){
+            console.log(item[Object.keys(item)[0]]);
+            //client.post("/movies",item[Object.keys(item)[0]],function(err,data){
+               // callback(data);
+            //});
+        }, function (err) {
+            console.log(err);
+        });
+        */
     }
 })
 
